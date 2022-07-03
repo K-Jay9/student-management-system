868b2c4192e2051d705fe2cac0dbe2ee040761b3
kjempire9 <kjempirecode@gmail.com>
2022-07-03 19:48:24 +0300

    Dummy data to populate the H2 database.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,151 @@
+
+INSERT INTO STUDENT (
+    STUDENT_NAME,
+    REG_NO,
+    COURSE_NAME,
+    COURSE_CODE,
+    HALL_NAME,
+    HALL_NUMBER,
+    DOB,
+    EMAIL,
+    JOINING,
+    GRADUATION
+)
+VALUES
+    (
+        'Joseph kamau',
+        'i20/123/17',
+        'Bsc Arts',
+        'i20',
+        'mamlaka',
+        '102',
+        '19890204',
+        'kamau@gmail.com',
+        '20170407',
+        '20210304'
+    );
+INSERT INTO STUDENT (
+    STUDENT_NAME,
+    REG_NO,
+    COURSE_NAME,
+    COURSE_CODE,
+    HALL_NAME,
+    HALL_NUMBER,
+    DOB,
+    EMAIL,
+    JOINING,
+    GRADUATION
+)
+VALUES
+    (
+        'Moses kipkorir',
+        'i63/3456/18',
+        'Bsc Statistics',
+        'i63',
+        'prefabs',
+        '117',
+        '19991114',
+        'kipkorir@gmail.com',
+        '20180407',
+        '20220304'
+    );
+INSERT INTO STUDENT (
+    STUDENT_NAME,
+    REG_NO,
+    COURSE_NAME,
+    COURSE_CODE,
+    HALL_NAME,
+    HALL_NUMBER,
+    DOB,
+    EMAIL,
+    JOINING,
+    GRADUATION
+)
+VALUES
+    (
+        'George Mulaney',
+        'i24/1156/16',
+        'Bsc Engineering',
+        'i24',
+        'Western Wing',
+        '135',
+        '19981212',
+        'mulaney@gmail.com',
+        '20160407',
+        '20220304'
+    );
+INSERT INTO STUDENT (
+    STUDENT_NAME,
+    REG_NO,
+    COURSE_NAME,
+    COURSE_CODE,
+    HALL_NAME,
+    HALL_NUMBER,
+    DOB,
+    EMAIL,
+    JOINING,
+    GRADUATION
+)
+VALUES
+    (
+        'Jimmy Carr',
+        'i20/125/13',
+        'Bsc Arts',
+        'i20',
+        'mamlaka B',
+        '103',
+        '19880409',
+        'carr@gmail.com',
+        '20130407',
+        '20170304'
+    );
+INSERT INTO STUDENT (
+    STUDENT_NAME,
+    REG_NO,
+    COURSE_NAME,
+    COURSE_CODE,
+    HALL_NAME,
+    HALL_NUMBER,
+    DOB,
+    EMAIL,
+    JOINING,
+    GRADUATION
+)
+VALUES
+    (
+        'Joseph karomo',
+        'i20/1258/17',
+        'Bsc Arts',
+        'i20',
+        'Mandela',
+        '001',
+        '19980601',
+        'karomo@gmail.com',
+        '20170407',
+        '20210304'
+    );
+INSERT INTO STUDENT (
+    STUDENT_NAME,
+    REG_NO,
+    COURSE_NAME,
+    COURSE_CODE,
+    HALL_NAME,
+    HALL_NUMBER,
+    DOB,
+    EMAIL,
+    JOINING,
+    GRADUATION
+)
+VALUES
+    (
+        'Michael Mburu',
+        'i21/13465/12',
+        'Bsc Assassin',
+        'i21',
+        'Layer',
+        '69',
+        '19690420',
+        'killer@gmail.com',
+        '20170407',
+        '20210304'
+    );
